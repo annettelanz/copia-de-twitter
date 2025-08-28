f206badc20d11f534f695f122aa8d79b48c34f76
unknown <annettelanz77@gmail.com>
2025-08-28 18:57:14 -0400

    detail_view

--- django_app/views.py
+++ django_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -8,3 +8,9 @@ class PostView(ListView):
     model =  Post
     template_name = 'home.html'
     #context_object_name = 'posts'
+
+
+class PostDetailView(DetailView) :
+    model = Post
+    template_name = 'description.html'
+    context_object_name = 'post'
